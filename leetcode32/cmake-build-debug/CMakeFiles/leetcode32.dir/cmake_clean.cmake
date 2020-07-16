c3d249626987aa5f653c768b7d21667423574d77
cwp <1075779382@qq.com>
2020-07-16 14:33:02 +0800

    最长的括号长度

---
+++ leetcode32/cmake-build-debug/CMakeFiles/leetcode32.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode32.dir/main.cpp.obj"
+  "leetcode32.exe"
+  "leetcode32.exe.manifest"
+  "leetcode32.lib"
+  "leetcode32.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- leetcode32/main.cpp
+++ leetcode32/main.cpp
@@ -1,6 +1,49 @@
-#include <iostream>
+/*
+ * name:Mr.Cai
+ * num:1
+ * 最长有效括号
+ * 算法思路：动态规划
+ * */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <stack>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+
+public:
+    int longestValidParentheses(string s) {
+        vector<int> dp(s.length(), 0);
+        int num = s.length();
+        int maxV = 0;
+        for (int i = 1; i < num; i++) {
+            if (s[i] == ')') {
+                if (s[i - 1] == '(') {
+                    dp[i] = 2;
+                    if (i - 2 > 0) {
+                        dp[i] += dp[i - 2];
+                    }
+                } else if (dp[i - 1] > 0) {
+                    if ((i - dp[i - 1] - 1) >= 0 && s[i - dp[i - 1] - 1] == '(') {
+                        dp[i] = dp[i - 1] + 2;
+                        if ((i - dp[i - 1] - 2) >= 0) {
+                            dp[i] += dp[i - dp[i - 1] - 2];
+                        }
+                    }
+                }
+            }
+            maxV = max(maxV, dp[i]);
+        }
+        return maxV;
+    }
+};
+int main()
+{
+    Solution s1;
+    string s = "(()";
+    cout<< s1.longestValidParentheses(s)<<endl;
+    return  0;
 }
